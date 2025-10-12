a2590c9b7125303bb4aedfb0d72c734059326fab
LeonardoRiera <159967149+LeonardoRiera@users.noreply.github.com>
2025-10-12 15:39:54 -0300

    section about us

--- src/components/Nosotros/Nosotros.jsx
+++ src/components/Nosotros/Nosotros.jsx
@@ -8,6 +8,7 @@ import logo3 from "../../assets/white-astron.png";
 import logo4 from "../../assets/white-tracker.png";
 import logo5 from "../../assets/white-it.png";
 import logo6 from "../../assets/white-profile.png";
+import nosotros from "../../assets/nosotros.webp";
 
 const Nosotros = () => {
   return (
@@ -23,42 +24,47 @@ const Nosotros = () => {
         <img src={logo6} alt="Logo cliente 6" />
       </div>
 
-      <div
-        className="nosotros-hero">
-        {/* // style={{ backgroundImage: `url(${nosotrosImg})` }} */}
-      
-        <h2>Trabajemos juntos en tu <br/> presencia online.</h2>
-        <h3>Somos Brenda y Leo. Artistas y desarrolladores web. <br/>
-            Fusionamos diseño gráfico y UX/UI para transformar ideas en experiencias digitales.
-            {/* </h3> */}
-            {/* <h3>Fusionamos diseño gráfico y UX/UI para transformar ideas en experiencias digitales. <br/> */}
-            {/*  <span className="span-nosotros">Podemos hacer grandes cosas juntos.</span>  */}
-            </h3>
-       
-        {/* Botón desktop */}
-        <div className="navbar-cta">
+      <div className="nosotros-hero">
+        <div className="card-text">
+          <h2 className="titulo-card">Trabajemos juntos en tu presencia online.</h2>
+          <h3>Somos Brenda y Leo. Artistas y desarrolladores web. <br/>Fusionamos diseño gráfico y UX/UI para transformar ideas en experiencias digitales.</h3> 
+
+          <Button
+            texto={<> Hablemos de tu Proyecto</>}
+            tipo="primario"
+            onClick={() => window.open('https://wa.me/tu-numero', '_blank')}
+            className="navbar-cta"
+          />
+         
+        </div>
+        
+        <img src={nosotros} alt="nosotros" className="nosotros-img" />
+ 
+      </div>
+
+      <div className="nosotros-texto-extra">
+          <p>
+              En Treno Digital reafirmamos el compromiso que nos ha acompañado a lo largo de los años:
+              crear, crecer y acompañar a quienes confían en nosotros. Este nuevo proyecto nace del mismo
+              espíritu con el que trabajamos siempre — dedicación, constancia y una búsqueda permanente
+              de calidad y evolución.
+          </p>
+      </div>
+    </section>
+  );
+};
+
+
+
+{/* Botón desktop */}
+        {/* <div className="navbar-cta">
           <Button
             texto={<> Hablemos de tu Proyecto</>}
             tipo="primario"
             onClick={() => window.open('https://wa.me/tu-numero', '_blank')}
           />
-        </div>
-
-        {/* <div>
-          <button className="btn secondary">Nuestros Servicios</button>
         </div> */}
-      </div>
 
-        <div className="nosotros-texto-extra">
-            <p>
-                En Treno Digital reafirmamos el compromiso que nos ha acompañado a lo largo de los años:
-                crear, crecer y acompañar a quienes confían en nosotros. Este nuevo proyecto nace del mismo
-                espíritu con el que trabajamos siempre — dedicación, constancia y una búsqueda permanente
-                de calidad y evolución.
-            </p>
-        </div>
-    </section>
-  );
-};
+
 
 export default Nosotros;
